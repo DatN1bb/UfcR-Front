27a2dd17a35b295088f53b6599d31079fe77b7c3
Nick Dew <franjogames15@gmail.com>
2025-03-28 21:19:06 +0100

    orders

---
+++ src/api/Order.ts
@@ -0,0 +1,15 @@
+import { apiRoutes } from 'constants/apiConstants' 
+import { OrderType} from 'models/order'
+import { apiRequest } from './Api'
+
+export const fetchorders = async (pageNumber: number) => 
+    apiRequest<undefined, OrderType[]>(
+'get',
+`${apiRoutes.ORDERS_PREFIX}?page=${pageNumber}`,
+    )
+
+export const fetchChart = async () =>
+apiRequest<undefined, { date: string; sum: string }[]>(
+'get',
+`${apiRoutes.ORDERS_PREFIX}/chart`,
+)
